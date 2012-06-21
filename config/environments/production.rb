dfb944ba59dd9fda45e7fce5c1de87ccc6fa9336
face4 <face4ad@gmail.com>
2012-06-21 09:55:38 +0800

    fix the assets precompile setting bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Face4So::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( users.js, posts.js, categories.js, posts.css, categories.css, application.js, front.css, users.css )
+  config.assets.precompile += %w( users.js posts.js categories.js posts.css categories.css application.js front.css users.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
